7daf6923fd98690c4c196ccd9d57b0ca5e7e8504
Nathan Letwory <nathan@mcneel.com>
2019-02-25 09:48:13 +0200

    Make plug-in load at startup.

--- RhinoPbrMaterial/Properties/AssemblyInfo.cs
+++ RhinoPbrMaterial/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using Rhino.PlugIns;
 // Make compatible with Rhino Installer Engine
 [assembly: AssemblyInformationalVersion("2")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
--- RhinoPbrMaterial/RhinoPbrMaterialPlugIn.cs
+++ RhinoPbrMaterial/RhinoPbrMaterialPlugIn.cs
@@ -1,4 +1,6 @@
-﻿namespace RhinoPbrMaterial
+﻿using Rhino.PlugIns;
+
+namespace RhinoPbrMaterial
 {
 	///<summary>
 	/// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
@@ -22,8 +24,6 @@
 			get; private set;
 		}
 
-		// You can override methods here to change the plug-in behavior on
-		// loading and shut down, add options pages to the Rhino _Option command
-		// and maintain plug-in wide options in a document.
+		public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
 	}
 }
